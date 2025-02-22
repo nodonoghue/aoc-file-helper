bae20a5be4607b516eff89f91bb4872328681be7
Nathan O'Donoghue <nodonoghue@protonmail.com>
2025-02-21 17:09:41 -0800

    More bugs, caused by not paying attention.

--- aocfilehelper.go
+++ aocfilehelper.go
@@ -1,6 +1,6 @@
 package filehelper
 
-import "github.com/nodonoghue/aoc-file-helper/internal/utils"
+import "github.com/nodonoghue/filehelper/internal/utils"
 
 // Pass in a file path to get a slice of file lines to pass through the parsing func of your choice
 func ReadFileLines(filename string) []string {
